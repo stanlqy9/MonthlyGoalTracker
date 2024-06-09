6ac16a0b4f024f1d981f8af05197e34d83656e8c
Stanley <stanlqqy@gmail.com>
2024-06-09 11:23:36 -0700

    created a monthly goal tracker

---
+++ challenges/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home_page),
+    path("<int:month>", views.monthly_challenges_by_number),
+    path("<str:month>", views.monthly_challenge),
+]
--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
-# Create your views here.
+monthly_challenges = {
+    "january": "learn python, learn algorithms and data structures, learn django, and flask",
+    "february": "maybe learn java? IDK YET",
+    "march": "march",
+    "april": "this is april",
+}
+
+def home_page(request):
+    return HttpResponse("<ul><li>January</li><li>February</li></ul>")
+
+def monthly_challenges_by_number(request, month):
+    try:
+        requested_month = list(monthly_challenges.keys())[month - 1]
+        return HttpResponseRedirect("/" + requested_month)
+    except:
+        return HttpResponseNotFound("<h1>404 page not found, idiot</h1>")
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("<h1>404 page not found, idiot</h1>")
+
